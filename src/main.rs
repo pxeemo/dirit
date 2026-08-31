b7567f2e0e904b8dc134a55d320a5db85b1d6a2a
Shayan <pxeemo@gmail.com>
2026-09-01 01:20:39 +0330

    refactor: make it modular

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,31 @@ struct Rename {
     temporary: PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let entries = std::fs::read_dir(".")?;
-    let mut entries_vec = Vec::new();
-    for (index, entry) in entries.enumerate() {
+fn get_entries() -> Result<Vec<Entry>, Box<dyn std::error::Error>> {
+    let dir_list = std::fs::read_dir(".")?;
+    let mut entries = Vec::new();
+    for (index, entry) in dir_list.enumerate() {
         let entry = entry?;
-        entries_vec.push(Entry {
+        entries.push(Entry {
             id: index + 1,
             path: entry.path(),
         });
     }
+    Ok(entries)
+}
 
+fn create_edit_file(entries: &[Entry]) -> Result<PathBuf, Box<dyn std::error::Error>> {
     let edit_path = "/tmp/dirit.txt";
     let mut file = std::fs::File::create(edit_path)?;
-    for entry in &entries_vec {
+    for entry in entries.iter() {
         writeln!(file, "{}\t{}", entry.id, entry.path.display())?;
     }
+    Ok(PathBuf::from(edit_path))
+}
 
-    let editor = std::env::var("EDITOR")?;
-    std::process::Command::new(editor).arg(edit_path).status()?;
-
-    let contents = std::fs::read_to_string(edit_path)?;
-    let mut edited_entries = Vec::new();
+fn parse_edited_entries(edit_path: &PathBuf) -> Result<Vec<Entry>, Box<dyn std::error::Error>> {
+    let contents = std::fs::read_to_string(&edit_path)?;
+    let mut entries = Vec::new();
     let mut paths = HashSet::new();
     for line in contents.lines() {
         let (id, path) = match line.split_once('\t') {
@@ -50,11 +53,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ))
             .into());
         }
-        edited_entries.push(Entry { id, path });
+        entries.push(Entry { id, path });
     }
+    Ok(entries)
+}
 
+fn delete_files(paths: &[PathBuf]) -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: fallback to rm
+    for path in paths {
+        let status = std::process::Command::new("trash-put")
+            .arg(&path)
+            .status()?;
+
+        if !status.success() {
+            return Err(format!("failed to trash {}", path.display()).into());
+        }
+        println!("Delete: {}", path.display());
+    }
+    Ok(())
+}
+
+fn rename_files(renames: &[Rename]) -> Result<(), Box<dyn std::error::Error>> {
+    for rename in renames.iter() {
+        std::fs::rename(&rename.from, &rename.temporary)?;
+    }
+    for rename in renames.iter() {
+        std::fs::rename(&rename.temporary, &rename.to)?;
+        println!(
+            "Rename: {} -> {}",
+            rename.from.display(),
+            rename.to.display()
+        );
+    }
+    Ok(())
+}
+
+fn process_edited_entries(
+    entries: &[Entry],
+    edited_entries: &[Entry],
+) -> Result<(), Box<dyn std::error::Error>> {
     let mut renames = Vec::new();
-    for (index, entry) in entries_vec.iter().enumerate() {
+    let mut deletes = Vec::new();
+    for (index, entry) in entries.iter().enumerate() {
         let edited = edited_entries.iter().find(|edited| edited.id == entry.id);
         match edited {
             Some(new) => {
@@ -66,31 +106,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     });
                 }
             }
-            None => {
-                let status = std::process::Command::new("trash-put")
-                    .arg(&entry.path)
-                    .status()?;
-
-                if !status.success() {
-                    return Err(format!("failed to trash {}", entry.path.display()).into());
-                }
-                println!("Delete: {}", entry.path.display())
-            }
+            None => deletes.push(entry.path.clone()),
         }
     }
+    rename_files(&renames)?;
+    delete_files(&deletes)?;
+    Ok(())
+}
 
-    for rename in &renames {
-        std::fs::rename(&rename.from, &rename.temporary)?;
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let entries = get_entries()?;
+    let edit_path = create_edit_file(&entries)?;
+    // TODO: get editor properly
+    let editor = std::env::var("EDITOR")?;
+    std::process::Command::new(editor)
+        .arg(&edit_path)
+        .status()?;
 
-    for rename in &renames {
-        std::fs::rename(&rename.temporary, &rename.to)?;
-        println!(
-            "Rename: {} -> {}",
-            rename.from.display(),
-            rename.to.display()
-        );
-    }
-
+    let edited_entries = parse_edited_entries(&edit_path)?;
+    process_edited_entries(&entries, &edited_entries)?;
     Ok(())
 }
