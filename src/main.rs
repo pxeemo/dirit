c0dcea81588c0a15cdd7da7a2ef65b6ef7042c21
Shayan <pxeemo@gmail.com>
2026-09-01 13:55:54 +0330

    feat: implement cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use clap::Parser;
 use std::collections::HashSet;
+use std::io::IsTerminal;
+use std::io::Read;
 use std::io::Write;
 use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "dirit", version)]
+struct Args {
+    paths: Vec<PathBuf>,
+    #[arg(long)]
+    dry_run: bool,
+}
 
 struct Entry {
     id: usize,
@@ -13,13 +24,13 @@ struct Rename {
     temporary: PathBuf,
 }
 
-fn get_entries() -> Result<Vec<Entry>, Box<dyn std::error::Error>> {
-    let dir_list = std::fs::read_dir(".")?;
+fn get_entries(dir: &PathBuf, offset_id: usize) -> Result<Vec<Entry>, Box<dyn std::error::Error>> {
+    let dir_list = std::fs::read_dir(dir)?;
     let mut entries = Vec::new();
     for (index, entry) in dir_list.enumerate() {
         let entry = entry?;
         entries.push(Entry {
-            id: index + 1,
+            id: index + offset_id + 1,
             path: entry.path(),
         });
     }
@@ -30,7 +41,11 @@ fn create_edit_file(entries: &[Entry]) -> Result<PathBuf, Box<dyn std::error::Er
     let edit_path = "/tmp/dirit.txt";
     let mut file = std::fs::File::create(edit_path)?;
     for entry in entries.iter() {
-        writeln!(file, "{}\t{}", entry.id, entry.path.display())?;
+        if entry.id == 0 {
+            writeln!(file, "{}", entry.path.display())?;
+        } else {
+            writeln!(file, "{}\t{}", entry.id, entry.path.display())?;
+        }
     }
     Ok(PathBuf::from(edit_path))
 }
@@ -112,6 +127,9 @@ fn process_edited_entries(
     let mut renames = Vec::new();
     let mut deletes = Vec::new();
     for (index, entry) in entries.iter().enumerate() {
+        if entry.id == 0 {
+            continue;
+        }
         let edited = edited_entries.iter().find(|edited| edited.id == entry.id);
         match edited {
             Some(new) => {
@@ -132,7 +150,47 @@ fn process_edited_entries(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let entries = get_entries()?;
+    let args = Args::parse();
+    let mut entries = Vec::new();
+    let mut last_id = 0;
+    for path in &args.paths {
+        if path.is_dir() {
+            let new_entries = get_entries(&path, last_id)?;
+            last_id += new_entries.len();
+            entries.extend(new_entries);
+        } else {
+            entries.push(Entry {
+                id: if path.exists() {
+                    last_id += 1;
+                    last_id
+                } else {
+                    0
+                },
+                path: path.clone(),
+            });
+        }
+    }
+    if !std::io::stdin().is_terminal() {
+        let mut buffer = String::new();
+        std::io::stdin().read_to_string(&mut buffer)?;
+        let paths = buffer
+            .lines()
+            .map(|line| PathBuf::from(line))
+            .collect::<Vec<_>>();
+        for path in paths {
+            entries.push(Entry {
+                id: if path.exists() {
+                    last_id += 1;
+                    last_id
+                } else {
+                    0
+                },
+                path: path.clone(),
+            });
+        }
+    } else if args.paths.is_empty() {
+        entries.extend(get_entries(&PathBuf::from("."), last_id)?);
+    }
     let edit_path = create_edit_file(&entries)?;
     // TODO: get editor properly
     let editor = std::env::var("EDITOR")?;
