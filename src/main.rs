2f811f38818cc4f35945aee6927daa73864e9df8
Shayan <pxeemo@gmail.com>
2026-09-06 18:56:06 +0330

    fix: symlink after first depth recurse

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn recursive_read_dir(dir: &Path) -> Result<HashSet<PathBuf>, Box<dyn std::error
     for entry in std::fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
-        if path.is_dir() {
+        if path.is_dir() && !path.is_symlink() {
             paths.extend(recursive_read_dir(&path)?);
         } else {
             paths.insert(path.to_path_buf());
@@ -267,7 +267,7 @@ fn process_path_args(
     let mut paths = HashSet::<PathBuf>::new();
     let mut new_paths = HashSet::<PathBuf>::new();
     for path in &args.paths {
-        if args.recursive && path.is_dir() && !path.is_symlink() {
+        if args.recursive && path.is_dir() {
             paths.extend(recursive_read_dir(path)?);
         } else if path.exists() {
             paths.insert(path.clone());
@@ -283,7 +283,7 @@ fn process_path_args(
             .map(|line| PathBuf::from(line))
             .collect::<Vec<_>>();
         for path in stdin_paths {
-            if args.recursive && path.is_dir() && !path.is_symlink() {
+            if args.recursive && path.is_dir() {
                 paths.extend(recursive_read_dir(&path)?);
             } else if path.exists() {
                 paths.insert(path);
